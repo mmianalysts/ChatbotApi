4d6620907e82f1d5015c253bf06b114b4472ddca
Zhang-Zelong <zelong.zhang@moojing.com>
2024-09-09 17:01:50 +0800

    :bug: 修复stdout记录报错

--- src/utils.py
+++ src/utils.py
@@ -11,12 +11,13 @@ def log_completion_duration(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
         start = time.time()
-        reply = await func(*args, **kwargs)
+        reply, usage = await func(*args, **kwargs)
         duration = (time.time() - start) * 1000
         extra = {"duration": duration, "reply": reply}
         extra.update(zip(record_params, args))
         extra.update({k: v for k, v in kwargs.items() if k in record_params[len(args) :]})
         extra["prompt"] = extra.pop("text", "")
+        extra.update(usage.model_dump())
         logger.info("Prompt: {prompt}\nReply: {reply}", extra=extra)
         return reply
 
